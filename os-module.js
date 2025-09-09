5d446a0ab0cfbc48495ac752f6ba218bdfa5de5c
Walid <ow_bouhenika@esi.dz>
2025-09-09 16:33:21 +0100

    sure

--- os-module.js
+++ os-module.js
@@ -3,5 +3,6 @@ const CurrentOSInfo = {
 	type: os.type(),
 	Totalmemory: os.totalmem(),
 	freeMemory: os.freemem(),
+	UpTime: os.uptime(),
 };
 console.log(CurrentOSInfo);
---
+++ path-module.js
@@ -0,0 +1,3 @@
+const path = require("path");
+console.log(path.sep); // returns the path sepearator for current system
+console.log(path.join("/house", "noise", "box.txt")); // just creates a path from variables
