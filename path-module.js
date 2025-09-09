5362c08dfc36c0a1a0ff42ab284a5c103ac19aa3
Walid <ow_bouhenika@esi.dz>
2025-09-09 16:52:39 +0100

    ye

--- path-module.js
+++ path-module.js
@@ -1,3 +1,5 @@
 const path = require("path");
 console.log(path.sep); // returns the path sepearator for current system
 console.log(path.join("/house", "noise", "box.txt")); // just creates a path from variables
+pathtepm = path.join("/house", "noise", "box.txt");
+console.log(path.basename(pathtepm));
