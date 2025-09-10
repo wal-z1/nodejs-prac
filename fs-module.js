29d2f60df2d129226f9cd2b44d3365c871ae3cf0
Walid <ow_bouhenika@esi.dz>
2025-09-10 09:26:40 +0100

    writing to text files

--- fs-module.js
+++ fs-module.js
@@ -5,3 +5,8 @@ const TextOfSecondFile = readFileSync("./sub/test_text2.txt", "utf8");
 
 console.log(TextOfirstFile);
 console.log(TextOfSecondFile);
+
+writeFileSync(
+	"./sub/ConcatOfTheTwoFiles.txt",
+	`\nhere is the concat ${TextOfirstFile} + ${TextOfSecondFile} `
+);
