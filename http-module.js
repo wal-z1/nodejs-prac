f734e02f397404c6157a9e5268bffa502dbc1925
Walid <ow_bouhenika@esi.dz>
2025-09-10 18:10:33 +0100

    yes addede soemthing like a req or something

--- http-module.js
+++ http-module.js
@@ -1,4 +1,7 @@
 const http = require("http");
-const servertest = http.createServer((req,res)){
-
-}
+const servertest = http.createServer((req, res) => {
+  console.log(req)
+	res.write("Hello world");
+  res.end()
+});
+servertest.listen(5800)
