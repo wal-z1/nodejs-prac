116a67180392c8939391a60ea2912825eca84416
Walid <ow_bouhenika@esi.dz>
2025-09-10 09:04:43 +0100

    path

--- path-module.js
+++ path-module.js
@@ -3,3 +3,6 @@ console.log(path.sep); // returns the path sepearator for current system
 console.log(path.join("/house", "noise", "box.txt")); // just creates a path from variables
 pathtepm = path.join("/house", "noise", "box.txt");
 console.log(path.basename(pathtepm));
+console.log(__dirname);
+console.log(path.resolve(__dirname, "house.txt"));
+console.log(path.resolve("house.txt"));
