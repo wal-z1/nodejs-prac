16c22cc1503c338b761345c8556d59e977f4bebb
Walid <ow_bouhenika@esi.dz>
2025-09-12 11:39:00 +0100

    using promise for task takes time

--- asyncwritereadexmple.js
+++ asyncwritereadexmple.js
@@ -1,3 +1,16 @@
 const fs = require("fs");
 
-fs.readFile("./sub/test_text1.txt", "utf", (err, txt) => {});
+const getxt = (path) => {
+	return new Promise((yes, no) => {
+		fs.readFile(path, "utf8", (err, txt) => {
+			if (err) {
+				no(err);
+			} else {
+				yes(txt);
+			}
+		});
+	});
+};
+getxt("./sub/test_text1.txt")
+	.then((res) => console.log(res))
+	.catch((err) => console.log(err));
