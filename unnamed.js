518f3495f01f0a326271451c6557d936938df200
Walid <ow_bouhenika@esi.dz>
2025-09-12 10:30:10 +0100

    lag

--- unnamed.js
+++ unnamed.js
@@ -1,12 +1,17 @@
 const http = require("http");
 const servertest = http.createServer((req, res) => {
 	if (req.url === "/") {
+    // sync code that lags asf
+		for (let i = 0; i < 100000; i++) {
+			console.log(i);
+		}
 		res.write("Hello world MAIN");
+
+		res.end();
 	} else if (req.url === "/about") {
 		res.write("wassap ppl");
+		res.end();
 	}
-
-	res.end();
 });
 servertest.listen(5800, () => {
 	console.log("makima is listening");
