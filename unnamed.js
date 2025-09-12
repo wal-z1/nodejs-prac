4cd9e76709ba5359ed16751561ab0b92b53f9b0c
Walid <ow_bouhenika@esi.dz>
2025-09-12 10:10:27 +0100

    unamed js

---
+++ unnamed.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const servertest = http.createServer((req, res) => {
+	console.log(req);
+	if (req.url === "/") {
+		res.write("Hello world MAIN");
+		res.end();
+	} else if (req.url === "/second") {
+		res.write("Hello world second");
+		res.end();
+	}
+	res.end(`<h1>hey this page does not exist</h1>
+    <a href="/">Check here instead</a>`);
+});
+servertest.listen(5800);
+//
