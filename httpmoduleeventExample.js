7deb25a3d4c180f0131356452db5ca1f7705943f
Walid <ow_bouhenika@esi.dz>
2025-09-13 10:52:46 +0100

    you make it

--- httpmoduleeventExample.js
+++ httpmoduleeventExample.js
@@ -1,1 +1,8 @@
 // oh hello there
+const http = require("http")
+
+const myserver = http.createServer()
+//we create the server 
+
+myserver.on("request",(req,res)=>{res.end("kill it")})
+myserver.listen(5000)
