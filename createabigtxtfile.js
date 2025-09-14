5b837b98740edcfd5468c47b0af1b360aab5e0f8
Walid <ow_bouhenika@esi.dz>
2025-09-14 10:05:50 +0100

    created a big fiule to learn streams

--- createabigtxtfile.js
+++ createabigtxtfile.js
@@ -1,4 +1,4 @@
 const { writeFileSync } = require("fs");
 for (let i = 1; i < 100; i++) {
-	writeFileSync("./sub/numbersfile.txt", `Line ${i}`, { flag: "a" });
+	writeFileSync("./sub/numbersfile.txt", `Line ${i}\n`, { flag: "a" });
 }
