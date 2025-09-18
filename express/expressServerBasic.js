e034f1b7cb8ede12bf1ac58e4aa54fe99798f4fd
Walid <ow_bouhenika@esi.dz>
2025-09-18 09:52:06 +0100

    get method

---
+++ express/expressServerBasic.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.listen(5000, () => {
+	console.log("listening on port 5000");
+});
+
+//get method that is preformed by the browser
+
+app.get("/", (req, res) => {
+	res.send("hello");
+});
+app.get("/about", (req, res) => {
+	res.send("about pagen ahh");
+});
